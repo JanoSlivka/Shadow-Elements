b91ef8a54f416ce9cc4678c95cb44a440f641e2c
Bruno Gardlo <bruno.gardlo@gmail.com>
2019-02-18 12:41:52 +0100

    Adding possibility to test if the custom element works inside of angular application. Fixed quering for my-slot elements, so that it only selects child in the current host

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector, EventEmitter } from '@angular/core';
 import { createCustomElement, NgElement, WithProperties } from '@angular/elements';
+// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { WrapperComponent } from './wrapper/wrapper.component';
 import { SlotComponent } from './slot/slot.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { AppComponent } from './app.component';
 
 declare global {
-    interface HTMLElementTagNameMap {
-        'my-wrapper > my-slot': NgElement & WithProperties<{
-            action: EventEmitter<string>,
-            setInput(data: any): void
-        }>;
-        'my-wrapper': NgElement & WithProperties<{
-            wrapperData: any
-        }>;
-    }
+  interface HTMLElementTagNameMap {
+    'my-slot': NgElement & WithProperties<{
+      action: EventEmitter<string>,
+      setInput(data: any): void
+    }>;
+    'my-wrapper': NgElement & WithProperties<{
+      wrapperData: any
+    }>;
+  }
 }
 
 @NgModule({
-    imports: [
-        BrowserModule
-    ],
-    declarations: [
-        SlotComponent,
-        WrapperComponent
-    ],
-    entryComponents: [
-        SlotComponent,
-        WrapperComponent
-    ],
-    providers: [],
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    SlotComponent,
+    WrapperComponent,
+    // AppComponent,
+  ],
+  entryComponents: [
+    SlotComponent,
+    WrapperComponent,
+    // AppComponent
+  ],
+  // schemas: [
+  //     CUSTOM_ELEMENTS_SCHEMA
+  // ]
 })
 export class AppModule {
-    constructor(private injector: Injector) {}
+  constructor(private injector: Injector) { }
 
-    ngDoBootstrap() {
-        const slotComponent = createCustomElement(SlotComponent, {
-            injector: this.injector
-        });
-        const wrapperComponent = createCustomElement(WrapperComponent, {
-            injector: this.injector
-        });
+  ngDoBootstrap(app) {
+    const elements: any[] = [
+      [SlotComponent, 'my-slot'],
+      [WrapperComponent, 'my-wrapper'],
+    ];
 
-        customElements.define('my-slot', slotComponent);
-        customElements.define('my-wrapper', wrapperComponent);
+    for (const [component, name] of elements) {
+      const el = createCustomElement(component, { injector: this.injector });
+      customElements.define(name, el);
     }
+
+    // // create DOM element for the component being bootstrapped
+    // // and add it to the DOM
+    // const componentElement = document.createElement('app-root');
+    // document.body.appendChild(componentElement);
+    // // bootstrap the application with the component
+    // app.bootstrap(AppComponent);
+  }
 }
--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -1,69 +1,69 @@
 import {
-    AfterViewInit,
-    ChangeDetectionStrategy,
-    ChangeDetectorRef,
-    Component,
-    Input,
-    OnInit,
-    OnDestroy,
-    ViewEncapsulation
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  ViewEncapsulation
 } from '@angular/core';
 
 @Component({
-    templateUrl: './wrapper.component.html',
-    styleUrls: ['./wrapper.component.scss'],
-    encapsulation: ViewEncapsulation.ShadowDom,
-    changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: './wrapper.component.html',
+  styleUrls: ['./wrapper.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WrapperComponent implements AfterViewInit, OnDestroy, OnInit {
+export class WrapperComponent implements OnDestroy, OnInit {
 
-    @Input('wrapper-data') wrapperData: any;
-    clicks: 0;
-    private mySlotEl = document.querySelector('my-wrapper > my-slot');
+  @Input('wrapper-data') wrapperData: any;
+  clicks: 0;
+  private mySlotEl;
 
-    constructor(
-        private cd: ChangeDetectorRef
-    ) {}
+  constructor(
+    private cd: ChangeDetectorRef
+  ) { }
 
-    ngOnInit() {
-        console.log(this.wrapperData);
+  ngOnInit() { }
+
+  ngOnDestroy() {
+    if (this.mySlotEl) {
+      this.mySlotEl.removeEventListener('action', this.mySlotAction.bind(this));
+    }
+  }
+
+  changeInputData(data) {
+    this.wrapperData = data;
+    this.updateChildContent();
+    this.cd.detectChanges();
+  }
+
+  slotChange($event) {
+    const assigned = $event.target.assignedNodes();
+    if (assigned.length == 0) {
+      console.log('No elements in wrapper...');
+      return;
     }
 
-    ngAfterViewInit() {
-        this.updateChildContent();
-        if (this.mySlotEl) {
-            this.mySlotEl.addEventListener('action', this.mySlotAction.bind(this));
-        }
-    }
+    // Find my-slot in current wrapper (to search only in given host, not the whole document)
+    this.mySlotEl = assigned.filter(_ => _.localName == 'my-slot')[0];
+    console.log(this.mySlotEl);
+    console.log(this.wrapperData);
 
-    ngOnDestroy() {
-        if (this.mySlotEl) {
-            this.mySlotEl.removeEventListener('action', this.mySlotAction.bind(this));
-        }
+    if (this.mySlotEl) {
+      this.mySlotEl.addEventListener('action', this.mySlotAction.bind(this));
     }
+  }
 
-    changeInputData(data) {
-        this.wrapperData = data;
-        this.updateChildContent();
-        this.cd.detectChanges();
-    }
+  private mySlotAction(event) {
+    console.log(event.detail);
+    this.clicks = event.detail;
+    this.cd.detectChanges();
+  }
 
-    slotChange($event) {
-        const assigned = $event.target.assignedNodes();
-        if (assigned.length > 0) {
-            console.log('my slot changed', assigned[0]);
-        }
-    }
-
-    private mySlotAction(event) {
-        console.log(event.detail);
-        this.clicks = event.detail;
-        this.cd.detectChanges();
-    }
-
-    private updateChildContent() {
-        if (!this.mySlotEl) return;
-        this.mySlotEl.setInput(this.wrapperData);
-    }
+  private updateChildContent() {
+    if (!this.mySlotEl) return;
+    this.mySlotEl.setInput(this.wrapperData);
+  }
 
 }
